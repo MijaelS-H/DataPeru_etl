35782504dec71a50418a9d746757c88e7d62ef30
Alan <alanramirezb@gmail.com>
2020-09-29 13:06:23 -0300

    new version, including all feedback

--- etl/indicadores/inicio_pipeline.py
+++ etl/indicadores/inicio_pipeline.py
@@ -1,32 +1,40 @@
 import pandas as pd
 import os
 import csv
-
+from functools import reduce
 from bamboo_lib.logger import logger
 from bamboo_lib.helpers import grab_connector
 from bamboo_lib.models import EasyPipeline, PipelineStep, Parameter
 from bamboo_lib.steps import LoadStep
 
 VARIABLE_DICT = { 
-    1: "Tasa de desempleo",
-    2: "Población ocupada",
-    3: "Tasa de pobreza",
-    4: "Ingreso promedio mensual",
-    5: "Contribución al VAB",
-    6: "Variación del VAB",
-    7: "Porcentaje de MYPE",
-    8: "Oficinas financieras",
-    9: "Monto de depósitos",
-    10: "Créditos directos",
-    11: "MYPE accede a crédito inicial",
-    12: "MYPE accede a crédito",
-    13: "Investigadores cada 10 mil habitantes",
-    14: "Inversión en I+D",
-    15: "Exportaciones en valor FOB",
-    16: "Ejecución presupuestal",
-    17: "Ejecución presupuestal del ITP",
+    1: "tasa_desempleo",
+    2: "poblacion_ocupada",
+    3: "tasa_pobreza",
+    4: "ingreso_promedio_mensual",
+    5: "contribucion_al_vab",
+    6: "variacion_del_vab",
+    7: "porcentaje_de_mype",
+    8: "oficinas_financieras",
+    9: "monto_de_depositos",
+    10: "creditos_directos",
+    11: "mype_accede_a_credito_inicial",
+    12: "mype_accede_a_credito",
+    13: "investigadores_cada_10_mil_habitantes",
+    14: "inversion_en_id",
+    15: "exportaciones_en_valor_fob",
+    16: "ejecucion_presupuestal",
+    17: "ejecucion_presupuestal_del_itp",
 }
 
+ACCENTS_MAP = {
+    "Ancash": "Áncash",
+    "Apurimac": "Apurímac",
+    "Huanuco": "Huánuco",
+    "Junin": "Junín",
+    "Madre De Dios": "Madre de Dios",
+    "San Martin": "San Martín"
+}
 
 class OpenStep(PipelineStep):
     def run_step(self, prev, params):
@@ -83,94 +91,60 @@ class TidyStep(PipelineStep):
         df[12]['year'] = 2017
         df[13]['year'] = 2015
         df[15]['year'] = 2018
-        
         for i in range(1,18):
             df[i]["region"] = df[i]["region"].str.title()
-            df[i]["data_origin"] = "ENAHO - INEI" if i in [1,2,3,4] else "INEI" if i in [5,6,8,9,10] else "SUNAT" if i in [7,15] else  "ENE" if i in [11,12]  else "SIAF" if i in [16,17] else "CONCYTEC" if i in [13,14] else "N/A"
             df[i] = df[i].rename(columns={"año":"year",
+            "region" : 'department_name',
             "valor_porcentaje": "value",
             "valor_numerico": "value",
             "valor_numerico (millones)": "value",
             "valor_numerico (millones dolares)": "value",
             "valor_numerico (por cada 10 mil)" : "value"})
-            df[i]["variable"] = VARIABLE_DICT[i]
             df[i]["year"] = df[i]["year"].astype(int)
-        
-        
+            df[i] = df[i][['department_name','year','value']]    
+
         df[14]['value'] =  df[14]['value']*1000000
         df[15]['value'] =  df[15]['value']*1000000
-        
+
+        for i in range(1,18):
+             df[i] = df[i].rename(columns={"value": VARIABLE_DICT[i]})
+
         df_list = [df[i] for i in range(1,18)]
-        df = pd.concat(df_list, ignore_index=True)
-        df = df[['region','year','data_origin','variable','value']]
-        
-        return df
 
-class RegionDimensionStep(PipelineStep):
-    def run_step(self, prev, params):
-        logger.info("Creating Region Dimension...")
+        final_df = reduce(lambda df1,df2: pd.merge(df1,df2,on=['department_name', 'year'],how='outer'), df_list)
+        final_df['department_name'] = final_df['department_name'].replace(ACCENTS_MAP)
 
-        df = prev
+        return final_df     
 
-        region_list = list(df["region"].unique())
-        df_region = pd.DataFrame({"region_id": list(range(len(region_list))), "region_name": sorted(region_list)})
-        
-
-        return df, df_region
-
-class VariableDimensionStep(PipelineStep):
-    def run_step(self, prev, params):
-        logger.info("Creating Variable Dimension...")
-
-        df, df_region = prev
-        
-
-        df_var = df["variable"].copy()
-        df_var = df_var.drop_duplicates().reset_index(drop=True)
-        df_var = pd.DataFrame(df_var)
-        df_var["variable_id"] = df_var.index
-        
-
-        return  df, df_region, df_var
-
-class OriginDimensionStep(PipelineStep):
-    def run_step(self, prev, params):
-        logger.info("Creating Origin Dimension...")
-
-        df, df_region, df_var = prev
-
-        df_origin = df["data_origin"].copy()
-        df_origin = df_origin.drop_duplicates().reset_index(drop=True)
-        df_origin = pd.DataFrame(df_origin)
-        df_origin["data_origin_id"] = df_origin.index
-        
-        return df, df_region, df_var, df_origin
 
 
 class FactTableStep(PipelineStep):
     def run_step(self, prev, params):
         logger.info("Creating Fact Table...")
 
-        df, df_region, df_var, df_origin = prev
+        final_df = prev
         
+        regions = pd.DataFrame(final_df['department_name'].unique()).reset_index()
+        regions['index'] = regions['index'] + 1
+        regions_map = {k:v for (k,v) in zip(final_df['department_name'].unique(), regions['index'])}
+        final_df["department_id"] = final_df["department_name"].map(regions_map)
         
-        region_map = {k:v for (k,v) in zip(df_region["region_name"], df_region["region_id"])}
-        df["region_id"] = df["region"].map(region_map)
+
+        final_df["department_id"] = final_df["department_id"].astype(str)
+        final_df["year"] = final_df["year"].astype(int)
+
+        final_df = final_df[['department_id','year','tasa_desempleo', 'poblacion_ocupada', 'tasa_pobreza',
+       'ingreso_promedio_mensual', 'contribucion_al_vab', 'variacion_del_vab',
+       'porcentaje_de_mype', 'oficinas_financieras', 'monto_de_depositos',
+       'creditos_directos', 'mype_accede_a_credito_inicial',
+       'mype_accede_a_credito', 'investigadores_cada_10_mil_habitantes',
+       'inversion_en_id', 'exportaciones_en_valor_fob',
+       'ejecucion_presupuestal', 'ejecucion_presupuestal_del_itp']]
+    
         
-        
-        
-        origin_map = {k:v for (k,v) in zip(df_origin["data_origin"], df_origin["data_origin_id"])}
-        df["data_origin_id"] = df["data_origin"].map(origin_map)
-        
-        
-        variable_map = {k:v for (k,v) in zip(df_var["variable"], df_var["variable_id"])}
-                
-        df["variable_id"] = df["variable"].map(variable_map)
-        
-        df = df[["region_id", "year", "data_origin_id", "variable_id",  "value"]]
-        
-        
-        return df
+
+
+        return final_df
 
 
 class InicioPipeline(EasyPipeline):
@@ -188,50 +162,60 @@ class InicioPipeline(EasyPipeline):
         open_step = OpenStep()
         tidy_step = TidyStep()
 
-        region_step = RegionDimensionStep()
-        load_region = LoadStep(
-            table_name="inicio_dim_region", 
-            connector=db_connector, 
-            if_exists="drop", 
-            pk=["region_id"],
-            dtype={"region_id":"UInt8","region_name":"String"},
-            nullable_list=[]
-        )
-
-        variable_step = VariableDimensionStep()
-        load_variable = LoadStep(
-            table_name="inicio_dim_variable", 
-            connector=db_connector, 
-            if_exists="drop", 
-            pk=["variable_id"],
-            dtype={"variable_id":"UInt8","variable_name":"String"},
-            nullable_list=[]
-        )
-
-        origin_step = OriginDimensionStep()
-        load_origin = LoadStep(
-            table_name="inicio_dim_origin", 
-            connector=db_connector, 
-            if_exists="drop", 
-            pk=["data_origin_id"],
-            dtype={"data_origin_id":"UInt8","data_origin":"String"},
-            nullable_list=[]
-        )
 
         fact_step = FactTableStep()
         load_fact = LoadStep(
-            table_name="inicio_fact", 
+            table_name="itp_fact", 
             connector=db_connector, 
             if_exists="drop", 
-            pk=["region_id"],
-            dtype={"region_id":"UInt8","year":"UInt8","data_origin_id":"UInt8","variable_id":"UInt8","percentage":"Float64"},
-            nullable_list=[]
+            pk=["department_id"],
+            dtype={"department_id":"String",
+                        "year":"UInt32",
+                        "tasa_desempleo":"Float32",
+                        "poblacion_ocupada":"Float32",
+                        "tasa_pobreza":"Float32",
+                        "ingreso_promedio_mensual":"Float32",
+                        "contribucion_al_vab":"Float32",
+                        "variacion_del_vab":"Float32",
+                        "porcentaje_de_mype":"Float32",
+                        "oficinas_financieras":"UInt16 ",
+                        "monto_de_depositos":"Float32 ",
+                        "creditos_directos":"Float32",
+                        "mype_accede_a_credito_inicial":"Float32",  
+                        "mype_accede_a_credito":"Float32",  
+                        "investigadores_cada_10_mil_habitantes":"Float64",  
+                        "inversion_en_id":"Float32",  
+                        "exportaciones_en_valor_fob":"Float32",
+                        "ejecucion_presupuestal":"Float32",  
+                        "ejecucion_presupuestal_del_itp":"Float32",      
+                    },
+
+            
+            nullable_list = [
+                    "tasa_desempleo",
+                    "poblacion_ocupada",
+                    "tasa_pobreza",
+                    "ingreso_promedio_mensual",
+                    "contribucion_al_vab",
+                    "variacion_del_vab",
+                    "porcentaje_de_mype",
+                    "oficinas_financieras",
+                    "monto_de_depositos",
+                    "creditos_directos",
+                    "mype_accede_a_credito_inicial",  
+                    "mype_accede_a_credito",  
+                    "investigadores_cada_10_mil_habitantes",  
+                    "inversion_en_id",  
+                    "exportaciones_en_valor_fob",
+                    "ejecucion_presupuestal",  
+                    "ejecucion_presupuestal_del_itp",    
+                        ],
         )
 
         if params.get("ingest")==True:
-            steps = [open_step, tidy_step, region_step, load_region, variable_step, load_variable, origin_step, load_origin, fact_step, load_fact]
+            steps = [open_step, tidy_step, fact_step, load_fact]
         else:
-            steps = [open_step, tidy_step, region_step, variable_step, origin_step, fact_step]
+            steps = [open_step, tidy_step, fact_step]
 
         return steps
 
