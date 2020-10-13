6a4dd249a40cd970f3a7949eab9857b8617c8012
D2SD <samueloso21@gmail.com>
2020-10-13 17:07:17 -0300

    ene survey pipeline

---
+++ etl/survey_indicators/static.py
@@ -0,0 +1,17 @@
+
+COLUMNS_RENAME = {
+    'año': 'year',
+    'indicador': 'indicator_id',
+    'categoría': 'category_id'
+}
+
+INDUSTRY_REPLACE = {
+    'Pesca y acuicultura': 2,
+    'Explotación de minas y canteras': 3,
+    'Industrias manufactureras': 4,
+    'Suministro de electricidad, gas y agua': 5,
+    'Construcción': 6,
+    'Comercio': 7,
+    'Transporte y almacenamiento': 8,
+    'Servicios': 9
+}
