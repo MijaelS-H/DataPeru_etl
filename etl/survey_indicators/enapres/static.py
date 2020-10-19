8f69db917d731e0ab5a5e4fa2d0d7ac25fe65587
jelmy hermosilla <jelmyhermosilla@MacBook-Pro-de-jelmy.local>
2020-10-19 15:19:04 -0300

    creates enapres pipeline

---
+++ etl/survey_indicators/enapres/static.py
@@ -0,0 +1,22 @@
+COLUMNS_RENAME = {
+    'año': 'year',
+    'indicador': 'indicator_id',
+    'categoría': 'category_id'
+}
+
+DEPARTMENT_EXCEPTIONS = {
+    'Ancash': 'Áncash', 
+    'Apurimac':'Apurímac'
+}
+
+SECTOR_DICT = {
+    'Urbano': 1, 
+    'Rural': 2
+}
+
+LOCATION_DICT = {
+    'Costa': 1, 
+    'Sierra': 2, 
+    'Selva': 3
+}
+  
