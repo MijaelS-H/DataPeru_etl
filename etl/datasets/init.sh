3470fe3acdd79977662a7d13c4c07fa1192759f7
ffigueroal <felipe.figueroa.lara@gmail.com>
2020-10-21 20:11:42 -0300

    adds init file to framework variables pipelines

---
+++ etl/datasets/init.sh
@@ -0,0 +1,38 @@
+# itp_indicators_y_act_dept_pipeline
+python run.py
+
+# inei_population_y_age_nat_travel_pipeline
+python inei_population_y_age_nat_travel_pipeline.py
+
+# inei_population_y_gender_dep_pipeline
+python inei_population_y_gender_dep_pipeline.py
+
+# inei_population_y_gender_nat_travel_pipeline
+python inei_population_y_gender_nat_travel_pipeline.py
+
+# inei_population_y_n_dep_pipeline
+python inei_population_y_n_dep_pipeline.py
+
+# inei_population_y_n_dep_urb_rur_pipeline
+python inei_population_y_n_dep_urb_rur_pipeline.py
+
+# itp_indicators_m_n_nat_pipeline
+python itp_indicators_m_n_nat_pipeline.py
+
+# itp_indicators_q_n_nat_pipeline
+python itp_indicators_q_n_nat_pipeline.py
+
+# itp_indicators_y_n_nat_54_pipeline
+python itp_indicators_y_n_nat_54_pipeline.py
+
+# itp_indicators_y_n_nat_pipeline
+python itp_indicators_y_n_nat_pipeline.py
+
+# met_education_y_level_dep_pipeline
+python met_education_y_level_dep_pipeline.py
+
+# minsa_health_y_gender_age_nat_pipeline
+python minsa_health_y_gender_age_nat_pipeline.py
+
+# proinversion_fdi_y_origin_nat_pipeline
+python proinversion_fdi_y_origin_nat_pipeline.py
