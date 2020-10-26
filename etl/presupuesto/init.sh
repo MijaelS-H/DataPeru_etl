d104fdb406f1c8759fea862c10b864ec8cdd8698
D2SD <samueloso21@gmail.com>
2020-10-26 17:50:06 -0300

    minor changes init

--- etl/presupuesto/init.sh
+++ etl/presupuesto/init.sh
@@ -3,7 +3,6 @@
 python download_pipeline.py
 python temp_dims.py
 python dim_gastos_pipeline.py
-python dim_gastos_aggregate_pipeline.py
 python dim_ingresos_pipeline.py
 python presupuesto_gastos_pipeline.py
 python presupuesto_ingresos_pipeline.py
