ad1a4bee8b1b964466b2ddcb586f7036f3a671e9
EduardoVasquez <eduardovasquez42@gmail.com>
2020-10-26 15:55:27 -0300

    monthly perc morosity rate added

--- etl/datasets/itp_indicators_m_n_nat_pipeline.py
+++ etl/datasets/itp_indicators_m_n_nat_pipeline.py
@@ -17,6 +17,7 @@ class TransformStep(PipelineStep):
         # Loading data
         df1 = pd.read_excel(io = "{}/{}/{}".format(path, "A. Economía", "A.158.xlsx"), skiprows = (0,1,2))
         df2 = pd.read_excel(io = "{}/{}/{}".format(path, "A. Economía", "A.159.xlsx"), skiprows = (0,1,2))
+        df3 = pd.read_excel(io = "{}/{}/{}".format(path, "A. Economía", "A.166.xlsx"), skiprows = (0,1,2,4), usecols = "A:O")[0:12]
 
         # Common steps
         for item in [df1,df2]:
@@ -25,15 +26,22 @@ class TransformStep(PipelineStep):
             item["Mes"].replace(month_dict, inplace = True)
             item["month_id"] = item["Año"].astype(str) + item["Mes"].astype(str)
 
+        df3["Mes"].replace(month_dict, inplace = True)
+
         # Renaming columns to understandable names
         df1.rename(columns = {"Índice": "ipc_base_2011_observado", "Mensual": "ipc_base_2011_var_mes_anterior", "Acumulada": "ipc_base_2011_var_acumulado", "Anual": "ipc_base_2011_var_anio_anterior"}, inplace = True)
         df2.rename(columns = {"Índice": "ipm_base_2013_observado", "Mensual": "ipm_base_2013_var_mes_anterior", "Acumulada": "ipm_base_2013_var_acumulado"}, inplace = True)
+
+        df3 = pd.melt(df3, id_vars = ["Mes"], value_vars = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014,2015, 2016, 2017, 2018, 2019], var_name = "year", value_name = "perc_morosidad_credi_banca_mult")
+        df3["month_id"] = df3["year"].astype(str) + df3["Mes"]
 
         # Formatting data
         df1["ipc_base_2011_var_anio_anterior"].replace("-", pd.np.nan, inplace = True)
         df1 = df1[["month_id", "ipc_base_2011_observado", "ipc_base_2011_var_mes_anterior", "ipc_base_2011_var_acumulado", "ipc_base_2011_var_anio_anterior"]]
 
         df = pd.merge(df1,  df2[["month_id", "ipm_base_2013_observado", "ipm_base_2013_var_mes_anterior", "ipm_base_2013_var_acumulado"]], on = "month_id", how = "left")
+        df = pd.merge(df, df3[['month_id', 'perc_morosidad_credi_banca_mult']], on = 'month_id', how = 'left')
+
 
         for i in df.columns:
             df[i] = df[i].astype(float)
@@ -60,14 +68,15 @@ class itp_indicators_m_n_nat_pipeline(EasyPipeline):
             "ipc_base_2011_var_anio_anterior":                  "Float",
             "ipm_base_2013_observado":                          "Float",
             "ipm_base_2013_var_mes_anterior":                   "Float",
-            "ipm_base_2013_var_acumulado":                      "Float"
+            "ipm_base_2013_var_acumulado":                      "Float",
+            "perc_morosidad_credi_banca_mult":                  "Float"
             }
 
         transform_step = TransformStep()
         load_step = LoadStep(
             "itp_indicators_m_n_nat", db_connector, if_exists="drop", pk=["ubigeo"], dtype=dtype, 
             nullable_list=["ipc_base_2011_observado", "ipc_base_2011_var_mes_anterior", "ipc_base_2011_var_acumulado", "ipc_base_2011_var_anio_anterior", 
-            "ipm_base_2013_observado", "ipm_base_2013_var_mes_anterior", "ipm_base_2013_var_acumulado"]
+            "ipm_base_2013_observado", "ipm_base_2013_var_mes_anterior", "ipm_base_2013_var_acumulado", "perc_morosidad_credi_banca_mult"]
         )
 
         return [transform_step, load_step]
