4b5a6f427d8a1f58b303c83e665fc4bf3fce7ec6
D2SD <samueloso21@gmail.com>
2020-10-28 14:46:34 -0300

    requirements chromedriver

--- etl/presupuesto/download_pipeline.py
+++ etl/presupuesto/download_pipeline.py
@@ -2,13 +2,13 @@
 import os
 import time
 import json
-import requests
 import pandas as pd
 from static import DATA_FOLDER
+from selenium import webdriver
 
 os.makedirs('{}'.format(DATA_FOLDER), exist_ok=True)
 
-# presupuesto gastos
+# Presupuesto gastos
 df = pd.read_excel('https://docs.google.com/spreadsheets/d/e/2PACX-1vQTPqvMRgIPRjVJs4CGMbjbrQYtfxNgFRpTDtsUMNJXieR1VYd6cJ-O9TcGCsj2Vg/pub?output=xlsx')
 df.dropna(how='all', inplace=True)
 df.columns = ['source', 'sector', 'url']
@@ -26,11 +26,20 @@ for ele in df.to_dict(orient='records'):
         print('ERROR:', ele['source'], ele['url'])
     time.sleep(5)
 
-# presupuesto ingresos
+
+# Presupuesto ingresos
 urls = ['https://datosabiertos.mef.gob.pe/datasets/185602-comparacion-de-ingreso-del-gobierno-nacional-2014-2020.download',
        'https://datosabiertos.mef.gob.pe/datasets/185603-comparacion-de-ingreso-de-los-gobiernos-regionales-2014-2020.download',
        'https://datosabiertos.mef.gob.pe/datasets/185604-comparacion-de-ingreso-de-los-gobiernos-locales-2014-2020.download']
 
-for url in urls:
-    temp = pd.read_csv(url, encoding='latin-1')
-    temp.to_csv('{}/{}.csv'.format(DATA_FOLDER, url.split('/')[-1]), index=False)
+executable_path = os.getcwd() + '/chromedriver'
+
+for ele in urls:
+    options = webdriver.ChromeOptions()
+    prefs = {'download.default_directory' : DATA_FOLDER}
+    options.add_argument("--headless")
+    options.add_experimental_option('prefs', prefs)
+    driver = webdriver.Chrome(chrome_options=options, executable_path=executable_path)
+    driver.get(ele)
+
+driver.close
--- init.sh
+++ init.sh
@@ -3,3 +3,15 @@ sudo apt-get install python3-venv
 python3 -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# Chrome driver
+wget https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+
+# Clean
+rm chromedriver_linux64.zip
+rm google-chrome-stable_current_amd64.deb
