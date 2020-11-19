19424bf107932bfa7445f9b577fdb3958113f337
EduardoVasquez <eduardovasquez42@gmail.com>
2020-11-19 17:05:08 -0300

    deleted commented codes

--- etl/profile_dimensions/__init__.py
+++ etl/profile_dimensions/__init__.py
@@ -1,15 +1,8 @@
 from .dimension_ubigeo import run_pipeline as run_dimension_ubigeo_pipeline
 
 
-#from .inei_population_y_n_dep_urb_rur_pipeline import run_pipeline as run_pipeline_urban_rural_dep_department
-
-
-
 def run_pipeline(params: dict):
     run_dimension_ubigeo_pipeline(params)
-
-
-   # run_pipeline_urban_rural_dep_department(params)
 
 
 if __name__ == "__main__":
