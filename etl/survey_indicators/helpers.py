85d0aeff3f3396022d20319bf30812f8dfc5d933
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-04-08 16:50:29 -0400

    fixes helper function

--- etl/survey_indicators/helpers.py
+++ etl/survey_indicators/helpers.py
@@ -1,12 +1,11 @@
+
 import pandas as pd
-import xlrd
 
 def join_files(excel_file, excel_sheets, target_dim, target_dim_replace, encoding=None):
     # files
     temp = pd.DataFrame()
     for ele in excel_sheets:
-        wb = xlrd.open_workbook(excel_file, encoding_override=encoding)
-        df = pd.read_excel(wb, sheet_name=ele)
+        df = pd.read_excel(excel_file, sheet_name=ele)
         df.columns = df.columns.str.lower()
         df.rename(columns={target_dim: target_dim_replace}, inplace=True)
         temp = temp.append(df, sort=False)
