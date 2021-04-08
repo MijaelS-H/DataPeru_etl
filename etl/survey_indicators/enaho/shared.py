9c8a76a1b75c95eec793304e81faa75c5a8a7393
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-04-08 16:29:19 -0400

    removes test in shared file

--- etl/survey_indicators/enaho/shared.py
+++ etl/survey_indicators/enaho/shared.py
@@ -1,19 +1,15 @@
 
 from bamboo_lib.models import PipelineStep
 from bamboo_lib.helpers import query_to_df
-from bamboo_lib.connectors.models import Connector
 
 class ReplaceStep(PipelineStep):
     def run_step(self, prev, params):
-        
-        db_connector = Connector.fetch('clickhouse-database', open(params['connector']))
 
         df = prev
 
         # replace dims
         dim_geo_query = 'SELECT department_id, department_name FROM dim_shared_ubigeo_department'
-        #dim_geo = query_to_df(self.connector, raw_query=dim_geo_query)
-        dim_geo = query_to_df(db_connector, raw_query=dim_geo_query)
+        dim_geo = query_to_df(self.connector, raw_query=dim_geo_query)
         df['department_id'].replace(dict(zip(dim_geo['department_name'], dim_geo['department_id'])), inplace=True)
 
         category_dim = dict(zip(df['categoría'].unique(), range(1, len(df['categoría'].unique()) + 1 )))
