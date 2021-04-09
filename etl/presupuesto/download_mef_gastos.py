e4af7b59db6c5c0ed0bbd3262a3040ad900978d2
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-04-09 17:46:57 -0400

    modifies download mef pipelines

--- etl/presupuesto/download_mef_gastos.py
+++ etl/presupuesto/download_mef_gastos.py
@@ -1,27 +1,39 @@
 
-import csv
+import os
 import pandas as pd
-from io import StringIO
+from zipfile import ZipFile
 from bamboo_lib.connectors.models import Connector
 from bamboo_lib.models import EasyPipeline, Parameter, PipelineStep
-from bamboo_lib.steps import DownloadStep, UnzipStep, LoadStep
+from bamboo_lib.steps import DownloadStep, LoadStep
 from .static import URL_GASTO, GASTO_DTYPES_COLS
 from etl.helpers import clean_tables
+
+class UnzipStep(PipelineStep):
+    def run_step(self, prev, params):
+
+        """" "prev" returns the url path from the downloaded file at previous """
+
+        with ZipFile(prev, "r") as data:
+            print("Extracting {}".format(params.get("url")))
+            data.extractall(os.path.join(params.get("datasets"), "downloads"))
+
+        return True
 
 class TransformStep(PipelineStep):
     def run_step(self, prev, params):
 
         """ "prev" returns a list with ZipExtFile -> access the first (and only) file
             -> read the file (bytes) -> decode bytes -> parse to csv """
-        data = StringIO([x for x in prev][0].read().decode())
-        df = pd.read_csv(data)
+        #data = StringIO([x for x in prev][0].read().decode())
+
+        df = params.get("chunk")
 
         df.columns = df.columns.str.lower()
 
         # drop bad row at the end of the file
-        old_shape = df.shape[0]
+        # old_shape = df.shape[0]
         df.dropna(subset=["tipo_gobierno_nombre"], inplace=True)
-        assert old_shape - 1 == df.shape[0], "DROPNA ERROR!"
+        # assert old_shape - 1 == df.shape[0], "DROPNA ERROR!"
 
         df = df[["ano_eje", "mes_eje", "tipo_gobierno", "tipo_gobierno_nombre",
                 "division_funcional", "division_funcional_nombre",
@@ -54,12 +66,9 @@ class DownloadPipeline(EasyPipeline):
             Parameter(name="url", dtype=str),
             Parameter(name="force_download", dtype=bool)
         ]
-
+        
     @staticmethod
     def steps(params):
-        db_connector = Connector.fetch("clickhouse-database", open(params.get("connector")))
-
-        dtypes = GASTO_DTYPES_COLS
 
         download_step = DownloadStep(
             connector="mef",
@@ -67,36 +76,76 @@ class DownloadPipeline(EasyPipeline):
             force=params.get("force_download")
         )
 
-        unzip_step = UnzipStep(compression="zip")
+        unzip_step = UnzipStep()
+
+        return [download_step, unzip_step]
+
+class TempGastosPipeline(EasyPipeline):
+    @staticmethod
+    def steps(params):
+        db_connector = Connector.fetch("clickhouse-database", open(params.get("connector")))
+
+        dtypes = GASTO_DTYPES_COLS
 
         transform_step = TransformStep()
 
         load_step = LoadStep("temp_mef_gastos", db_connector, if_exists="append", pk=["district_id"], dtype=dtypes)
 
-        return [download_step, unzip_step, transform_step, load_step]
-
+        return(transform_step, load_step)
 
 def run_pipeline(params: dict):
     # drop table before run all
     clean_tables("temp_mef_gastos", params.get("connector"))
 
     pp = DownloadPipeline()
+    pp2 = TempGastosPipeline()
+
     for url in URL_GASTO[:-2]:
         pp_params = {"url": url, "force_download": False}
         pp_params.update(params)
         pp.run(pp_params)
+        
+        data = os.path.join(params.get("datasets"), "downloads", "{}.csv".format(url[:-4]))
+
+        print("Ingesting {}".format(data))
+
+        for chunk in pd.read_csv(data, iterator=True, chunksize=10**4):
+            pp2_params = {"chunk": chunk, "url": url}
+            pp2_params.update(params)
+            pp2.run(
+                pp2_params
+            )
+
+        print("Removing {}".format(data))
+
+        os.remove(data)
 
     # force download on the last 2 files
     for url in URL_GASTO[-2::]:
-        pp_params = {"url": url, "force_download": True}
+        pp_params = {"url": url, "force_download": False}
         pp_params.update(params)
         pp.run(pp_params)
 
+        data = os.path.join(params.get("datasets"), "downloads", "{}.csv".format(url[:-4]))
+
+        print("Ingesting {}".format(data))
+
+        for chunk in pd.read_csv(data, iterator=True, chunksize=10**4):
+            pp2_params = {"chunk": chunk, "url": url}
+            pp2_params.update(params)
+            pp2.run(
+                pp2_params
+            )
+
+        print("Removing {}".format(data))
+
+        os.remove(data)
 
 if __name__ == "__main__":
     import sys
     from os import path
     __dirname = path.dirname(path.realpath(__file__))
     run_pipeline({
-        "connector": path.join(__dirname, "..", "conns.yaml")
+        "connector": path.join(__dirname, "..", "conns.yaml"),
+        "datasets": sys.argv[1]
     })
--- etl/presupuesto/download_mef_ingresos.py
+++ etl/presupuesto/download_mef_ingresos.py
@@ -1,27 +1,39 @@
 
-import csv
+import os
 import pandas as pd
-from io import StringIO
+from zipfile import ZipFile
 from bamboo_lib.connectors.models import Connector
 from bamboo_lib.models import EasyPipeline, Parameter, PipelineStep
-from bamboo_lib.steps import DownloadStep, UnzipStep, LoadStep
+from bamboo_lib.steps import DownloadStep, LoadStep
 from .static import URL_INGRESO, INGRESO_DTYPES_COLS
 from etl.helpers import clean_tables
+
+class UnzipStep(PipelineStep):
+    def run_step(self, prev, params):
+
+        """" "prev" returns the url path from the downloaded file at previous """
+
+        with ZipFile(prev, "r") as data:
+            print("Extracting {}".format(params.get("url")))
+            data.extractall(os.path.join(params.get("datasets"), "downloads"))
+
+        return True
 
 class TransformStep(PipelineStep):
     def run_step(self, prev, params):
 
         """ 'prev' returns a list with ZipExtFile -> access the first (and only) file
             -> read the file (bytes) -> decode bytes -> parse to csv """
-        data = StringIO([x for x in prev][0].read().decode())
-        df = pd.read_csv(data)
+        # data = StringIO([x for x in prev][0].read().decode())
+
+        df = params.get("chunk")
 
         df.columns = df.columns.str.lower()
 
         # drop bad row at the end of the file
-        old_shape = df.shape[0]
+        #old_shape = df.shape[0]
         df.dropna(subset=["tipo_gobierno_nombre"], inplace=True)
-        assert old_shape - 1 == df.shape[0], "DROPNA ERROR!"
+        #assert old_shape - 1 == df.shape[0], "DROPNA ERROR!"
 
         # month_id
         df["month_id"] = (df["ano_doc"].astype(int).astype(str) + \
@@ -54,40 +66,76 @@ class DownloadPipeline(EasyPipeline):
 
     @staticmethod
     def steps(params):
-        db_connector = Connector.fetch("clickhouse-database", open(params.get("connector")))
-
-        dtypes = INGRESO_DTYPES_COLS
-
         download_step = DownloadStep(
             connector="mef",
             connector_path="etl/presupuesto/conns.yaml",
             force=params.get('force_download')
         )
 
-        unzip_step = UnzipStep(compression="zip")
+        unzip_step = UnzipStep()
+
+        return [download_step, unzip_step]
+
+class TempIngresosPipeline(EasyPipeline):
+    @staticmethod
+    def steps(params):
+        db_connector = Connector.fetch("clickhouse-database", open(params.get("connector")))
+
+        dtypes = INGRESO_DTYPES_COLS
 
         transform_step = TransformStep()
 
         load_step = LoadStep("temp_mef_ingresos", db_connector, if_exists="append", pk=["district_id"], dtype=dtypes)
 
-        return [download_step, unzip_step, transform_step, load_step]
-
+        return [transform_step, load_step]
 
 def run_pipeline(params: dict):
     # drop table before run all
     clean_tables("temp_mef_ingresos", params.get("connector"))
 
     pp = DownloadPipeline()
+    pp2 = TempIngresosPipeline()
+
     for url in URL_INGRESO[:-2]:
         pp_params = {"url": url, "force_download": False}
         pp_params.update(params)
         pp.run(pp_params)
 
+        data = os.path.join(params.get("datasets"), "downloads", "{}.csv".format(url[:-4]))
+
+        print("Ingesting {}".format(data))
+
+        for chunk in pd.read_csv(data, iterator=True, chunksize=10**4):
+            pp2_params = {"chunk": chunk, "url": url}
+            pp2_params.update(params)
+            pp2.run(
+                pp2_params
+            )
+
+        print("Removing {}".format(data))
+
+        os.remove(data)
+
     # force download on the last 2 files
     for url in URL_INGRESO[-2::]:
-        pp_params = {"url": url, "force_download": True}
+        pp_params = {"url": url, "force_download": False}
         pp_params.update(params)
         pp.run(pp_params)
+
+        data = os.path.join(params.get("datasets"), "downloads", "{}.csv".format(url[:-4]))
+
+        print("Ingesting {}".format(data))
+
+        for chunk in pd.read_csv(data, iterator=True, chunksize=10**4):
+            pp2_params = {"chunk": chunk, "url": url}
+            pp2_params.update(params)
+            pp2.run(
+                pp2_params
+            )
+
+        print("Removing {}".format(data))
+
+        os.remove(data)
 
 
 if __name__ == "__main__":
@@ -95,5 +143,6 @@ if __name__ == "__main__":
     from os import path
     __dirname = path.dirname(path.realpath(__file__))
     run_pipeline({
-        "connector": path.join(__dirname, "..", "conns.yaml")
+        "connector": path.join(__dirname, "..", "conns.yaml"),
+        "datasets": sys.argv[1]
     })
