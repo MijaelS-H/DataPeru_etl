e2c5f82c5d6f571dfe495b99ec5bad42e5d676e2
Michael Vasquez <mvasquez@itp.gob.pe>
2022-03-09 15:37:42 -0500

    Revert "RedCITE no reindexar 2"
    This reverts commit 7e910ffb43f75c3428a48fc80859a9755055caf6.

--- etl/shared/cite.py
+++ etl/shared/cite.py
@@ -50,7 +50,6 @@ class TransformStep(PipelineStep):
         cite_list = list(df['cite'].unique())
         #cite_map = {k:v for (k,v) in zip(sorted(cite_list), list(range(1, len(cite_list) +1)))}
         cite_map = {k:v for (k,v) in zip(cite_list, list(range(1, len(cite_list) +1)))}
-        
         df['cite_id'] = df['cite'].map(cite_map)
  
         df['cite_slug'] = df['cite'].str.lower()
