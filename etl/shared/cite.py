9b8a39348dd75291bbfcb1a736de2360ff5db48b
Michael Vasquez <mvasquez@itp.gob.pe>
2022-04-08 11:31:47 -0500

    Revert "Add CITEcusco"
    This reverts commit f69dae60ef8ee9af5f54ebbf08c58c85901cdb66.

--- etl/shared/cite.py
+++ etl/shared/cite.py
@@ -47,13 +47,12 @@ class TransformStep(PipelineStep):
         df['coordinador_ut'] = df['coordinador_ut'].fillna("No disponible")
         df['descriptivo'] = df['descriptivo'].str.lstrip()
 
-        #cite_list = list(df['cite'].unique())
+        cite_list = list(df['cite'].unique())
 
         #cite_map = {k:v for (k,v) in zip(sorted(cite_list), list(range(1, len(cite_list) +1)))}
-        #cite_map = {k:v for (k,v) in zip(cite_list, list(range(1, len(cite_list) +1)))}
+        cite_map = {k:v for (k,v) in zip(cite_list, list(range(1, len(cite_list) +1)))}
 
-        #df['cite_id'] = df['cite'].map(cite_map)
-        df['cite_id'] = df['CITE_ID']
+        df['cite_id'] = df['cite'].map(cite_map)
  
         df['cite_slug'] = df['cite'].str.lower()
         df['cite_slug'] = df['cite_slug'].apply(unidecode)
